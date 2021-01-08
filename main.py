394e5f7318f91a114bfb13d0c23d304da31efee9
onemanbuilds <72015638+onemanbuilds@users.noreply.github.com>
2021-01-08 05:50:27 +0100

    update
    - Added webhook support.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from colorama import init,Fore,Style
 from threading import Thread,Lock,active_count
 from sys import stdout
 from time import sleep
+from datetime import datetime
 import requests
 import json
 
@@ -55,13 +56,49 @@ class Main:
         self.use_proxy = config['use_proxy']
         self.proxy_type = config['proxy_type']
         self.threads_num = config['threads']
+        self.webhook_enable = config['webhook_enable']
+
+        self.webhook_url = config['webhook_url']
 
         print('')
 
         self.hits = 0
         self.bads = 0
         self.retries = 0
+        self.webhook_retries = 0
         self.lock = Lock()
+
+    def SendWebhook(self,title,message,icon_url,thumbnail_url,proxy,useragent):
+        try:
+            timestamp = str(datetime.utcnow())
+
+            message_to_send = {"embeds": [{"title": title,"description": message,"color": 65362,"author": {"name": "AUTHOR'S DISCORD SERVER [CLICK HERE]","url": "https://discord.gg/33UzcuY","icon_url": icon_url},"footer": {"text": "MADE BY ONEMANBUILDS","icon_url": icon_url},"thumbnail": {"url": thumbnail_url},"timestamp": timestamp}]}
+            
+            headers = {
+                'User-Agent':useragent,
+                'Pragma':'no-cache',
+                'Accept':'*/*',
+                'Content-Type':'application/json'
+            }
+
+            payload = json.dumps(message_to_send)
+
+            if self.use_proxy == 1:
+                response = requests.post(self.webhook_url,data=payload,headers=headers,proxies=proxy)
+            else:
+                response = requests.post(self.webhook_url,data=payload,headers=headers)
+
+            if response.text == "":
+                pass
+            elif "You are being rate limited." in response.text:
+                self.webhook_retries += 1
+                self.SendWebhook(title,message,icon_url,thumbnail_url,proxy,useragent)
+            else:
+                self.webhook_retries += 1
+                self.SendWebhook(title,message,icon_url,thumbnail_url,proxy,useragent)
+        except:
+            self.webhook_retries += 1
+            self.SendWebhook(title,message,icon_url,thumbnail_url,proxy,useragent)
 
     def GetRandomUserAgent(self):
         useragents = self.ReadFile('[Data]/useragents.txt','r')
@@ -96,22 +133,25 @@ class Main:
 
     def TitleUpdate(self):
         while True:
-            self.SetTitle(f'[One Man Builds Twitch Token Checker] ^| HITS: {self.hits} ^| BADS: {self.bads} ^| RETRIES: {self.retries} ^| THREADS: {active_count()-1}')
+            self.SetTitle(f'[One Man Builds Twitch Token Checker] ^| HITS: {self.hits} ^| BADS: {self.bads} ^| RETRIES: {self.retries} ^| WEBHOOK RETRIES: {self.webhook_retries} ^| THREADS: {active_count()-1}')
             sleep(0.1)
 
     def TokenCheck(self,token):
         try:
+            useragent = self.GetRandomUserAgent()
+
             headers = {
-                'User-Agent':self.GetRandomUserAgent(),
+                'User-Agent':useragent,
                 'Authorization':f'OAuth {token}'
             }
 
             response = ''
-
+            proxy = ''
             link = 'https://id.twitch.tv/oauth2/validate'
 
             if self.use_proxy == 1:
-                response = requests.get(link,headers=headers,proxies=self.GetRandomProxy())
+                proxy = self.GetRandomProxy()
+                response = requests.get(link,headers=headers,proxies=proxy)
             else:
                 response = requests.get(link,headers=headers)
 
@@ -128,6 +168,8 @@ class Main:
                 with open('[Data]/[Results]/bads.txt','a',encoding='utf8') as f:
                     f.write(token+'\n')
                 self.bads += 1
+                if self.webhook_enable == 1:
+                    self.SendWebhook('Twitch Token',token,'https://cdn.discordapp.com/attachments/776819723731206164/796935218166497352/onemanbuilds_new_logo_final.png','https://i.dlpng.com/static/png/4669469-twitch-logo-png-images-free-download-twitch-png-1600_1600_preview.png',proxy,useragent)
             else:
                 self.retries += 1
                 self.TokenCheck(token)
